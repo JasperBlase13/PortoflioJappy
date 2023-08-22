3390da0e56607cf9f744aa7fac409730b940d682
Jasper Dominic Blase <125795448+JasperBlase13@users.noreply.github.com>
2023-08-22 21:14:29 +0800

    Password Integration

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
-function firstBuild()
-{
-    alert("First Build. May subject to change \nJappy :)");
 
+
+let input1 = prompt("Admin or Guest?")
+let proceed = false;
+console.log(input1);
+
+
+if(input1){
+
+    while(input1)
+    {
+    input1 = input1.trim().toLowerCase();
+    if(input1 === "admin" )
+    {
+       let password = prompt("Enter the password");
+       password = password.trim().toLowerCase();
+        if(password === "riabel")
+        {
+            alert("Welcome to your page");
+            break;
+        }
+        else
+        {
+            alert("Wrong password");
+            input1 = false;
+            location.reload();
+        }
+
+        
+    }
+    else if(input1 === "guest")
+    {
+        alert("Welcome to my first build of portfolio website \nThis is subject to change.\n-Jappy")
+        break;
+    }
+
+    else
+    {
+        alert("Wrong input");
+        input1 = false;
+        location.reload();
+    }
+    }
 }
 
-firstBuild();
+else if(input1 === null)
+{
+    alert("You must enter either admin or student or close this tab if you don't want to see it \n-Jappy")
+    location.reload();
+}
